cb0bbb026fc9953d8452eb6d309ed2c7c43074e2
Bob Glickstein <bobg@emphatic.com>
2018-01-01 22:34:38 -0800

    Find maildir messages in both cur and new subdirs.

--- maildir/maildir.go
+++ maildir/maildir.go
@@ -16,23 +16,34 @@ type Maildir struct {
 }
 
 func New(name string) (*Maildir, error) {
-	cur := path.Join(name, "cur")
-	infos, err := ioutil.ReadDir(cur)
+	files, err := getfiles(name, "cur")
 	if err != nil {
 		return nil, err
 	}
-	var files []string
-	for _, info := range infos {
-		files = append(files, info.Name())
+	files2, err := getfiles(name, "new")
+	if err != nil {
+		return nil, err
 	}
-	return &Maildir{name, files}, nil
+	return &Maildir{name, append(files, files2...)}
+}
+
+func getfiles(dir, subdir string) ([]string, error) {
+	infos, err := ioutil.ReadDir(path.Join(dir, subdir))
+	if err != nil {
+		return nil, err
+	}
+	var result []string
+	for _, info := range infos {
+		files = append(files, path.Join(subdir, info.Name()))
+	}
+	return files, nil
 }
 
 func (m *Maildir) Message() (io.Reader, func() error, error) {
 	if len(m.files) == 0 {
 		return nil, nil, nil
 	}
-	file := path.Join(m.name, "cur", m.files[0])
+	file := path.Join(m.name, m.files[0])
 	m.files = m.files[1:]
 	f, err := os.Open(file)
 	if err != nil {
